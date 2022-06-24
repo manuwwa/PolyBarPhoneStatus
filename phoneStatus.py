24b2a3749cba69570391aeea77f2394ed506f18f
Marcin <manuwwa@gmail.com>
2022-06-24 23:31:35 +0200

    upload initial code

---
+++ phoneStatus.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import json
+import http.client
+USERNAME="pewdiepie" #replace with channel username
+
+file = open('/home/manuwwa/.config/polybar/homeAssistant.json')
+fileData = json.load(file)
+
+conn = http.client.HTTPSConnection(""+fileData["homeAssistantUrl"])
+
+payload = ""
+
+headers = {
+    'Content-Type': "application/json",
+    'Authorization': "Bearer "+fileData["token"]
+    }
+
+conn.request("GET", "/api/states/"+fileData["bateryLevelEntityId"], payload, headers)
+
+res = conn.getresponse()
+data = res.read()
+state = json.loads(data)["state"]
+
+conn.request("GET", "/api/states/"+fileData["batteryStatusEntityyId"], payload, headers)
+
+res = conn.getresponse()
+data = res.read()
+charging = json.loads(data)["state"]
+
+if charging == "charging":
+    print("\uf3cd\uf0e7 {:,d}%".format(int(state)))
+else :
+    print("\uf3cd {:,d}%".format(int(state)))    
